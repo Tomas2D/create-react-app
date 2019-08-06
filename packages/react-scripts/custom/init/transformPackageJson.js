45e824e2730970f43a7a21e59beb9112a2db82aa
Jiří Čermák <jiri.cermak@ackee.cz>
2019-08-06 16:54:32 +0200

    Add script for merging package.json files

--- packages/react-scripts/custom/init/transformPackageJson.js
+++ packages/react-scripts/custom/init/transformPackageJson.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const fs = require('fs-extra');
-
 const paths = require('./paths');
 
 module.exports = async function transformPackageJson() {
@@ -11,5 +10,20 @@ module.exports = async function transformPackageJson() {
     fs.readJSON(paths.customPackgeJson),
   ]);
 
-  // TODO: merge customPackageJson to packageJson
+  const finalPackageJson = {
+    ...packageJson,
+    ...customPackgeJson,
+    dependencies: {
+      ...packageJson.dependencies,
+      ...customPackgeJson.dependencies,
+    },
+    scripts: {
+      ...packageJson.scripts,
+      ...customPackgeJson.scripts,
+    },
+  };
+
+  const formattedFileContent = JSON.stringify(finalPackageJson, null, 2);
+
+  await fs.writeFile(paths.packageJson, formattedFileContent);
 };
