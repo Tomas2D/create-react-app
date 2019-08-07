8a94461f50eac1d56790bf438dba28d17032f4c3
Jiří Čermák <jiri.cermak@ackee.cz>
2019-08-07 17:03:46 +0200

    Add eject script modifier

---
+++ packages/react-scripts/custom/scripts/eject/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const sortByKey = require('../utils/sortByKey');
+
+// TODO:
+exports.modifyPackageJson = appPackage => {
+  //   appPackage.dependencies = sortByKey(appPackage.dependencies);
+  //   appPackage.devDependencies = sortByKey(appPackage.devDependencies);
+};
