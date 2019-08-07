8e0c64d3f758c6f9637b246eb54111e942d4cd03
Jiří Čermák <jiri.cermak@ackee.cz>
2019-08-07 17:05:35 +0200

    Customize build script

--- packages/react-scripts/scripts/build.js
+++ packages/react-scripts/scripts/build.js
@@ -8,9 +8,21 @@
 // @remove-on-eject-end
 'use strict';
 
+// @ackee/react-scripts - beginning
+const {
+  getTargetValue,
+  getNodeEnv,
+  getBuildEnv,
+} = require('../custom/scripts/build/utils/env');
+const targetValue = getTargetValue(process.argv.slice(2));
+const NODE_ENV = getNodeEnv(targetValue);
+const BUILD_ENV = getBuildEnv(targetValue);
+
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+process.env.BABEL_ENV = NODE_ENV;
+process.env.NODE_ENV = NODE_ENV;
+process.env.BUILD_ENV = BUILD_ENV;
+// @ackee/react-scripts - end
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -42,6 +54,9 @@ const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
 const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
 const printBuildError = require('react-dev-utils/printBuildError');
+// @ackee/react-scripts - beginning
+const bfj = require('bfj');
+// @ackee/react-scripts - end
 
 const measureFileSizesBeforeBuild =
   FileSizeReporter.measureFileSizesBeforeBuild;
@@ -58,6 +73,10 @@ const isInteractive = process.stdout.isTTY;
 if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
   process.exit(1);
 }
+
+// Process CLI arguments
+const argv = process.argv.slice(2);
+const writeStatsJson = argv.indexOf('--stats') !== -1;
 
 // Generate configuration
 const config = configFactory('production');
@@ -189,6 +208,20 @@ function build(previousFileSizes) {
         );
         return reject(new Error(messages.warnings.join('\n\n')));
       }
+
+      // @ackee/react-scripts - beginning
+      const resolveArgs = {
+        stats,
+        previousFileSizes,
+        warnings: messages.warnings,
+      };
+      if (writeStatsJson) {
+        return bfj
+          .write(paths.appBuild + '/bundle-stats.json', stats.toJson())
+          .then(() => resolve(resolveArgs))
+          .catch(error => reject(new Error(error)));
+      }
+      // @ackee/react-scripts - end
 
       return resolve({
         stats,
