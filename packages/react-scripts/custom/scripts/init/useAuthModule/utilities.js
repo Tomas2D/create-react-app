7da0cb64a952f2d3f72421379e098ace7e916130
Jiří Čermák <jiri.cermak@ackee.cz>
2019-08-12 18:25:16 +0200

    init/useAuthModule: fix path to dependency files

--- packages/react-scripts/custom/scripts/init/useAuthModule/utilities.js
+++ packages/react-scripts/custom/scripts/init/useAuthModule/utilities.js
@@ -7,7 +7,7 @@ const fileExists = file =>
     fs.access(file, fs.constants.F_OK, err => res([!err, file]));
   });
 
-exports.getValidFiles = async (src = []) => {
+exports.getExistingFiles = async (src = []) => {
   const results = await Promise.all(src.map(fileExists));
 
   return results.filter(result => result[0]).map(result => result[1]);
