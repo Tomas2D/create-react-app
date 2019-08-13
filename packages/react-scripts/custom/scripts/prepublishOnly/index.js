b8841036e9e419dcb1d3567d52557e19efab20ba
Jiří Čermák <jiri.cermak@ackee.cz>
2019-08-13 14:17:56 +0200

    Add *publish scripts

--- packages/react-scripts/custom/scripts/prepublishOnly/index.js
+++ packages/react-scripts/custom/scripts/prepublishOnly/index.js
@@ -1,7 +1,12 @@
 'use strict';
 
+const fs = require('fs-extra');
+const paths = require('../../config/paths');
+
 const transformPackageJson = require('./transformPackageJson');
 
 (async function() {
+  await fs.copy(paths.root, paths.temp.root);
+
   await transformPackageJson();
 })();
