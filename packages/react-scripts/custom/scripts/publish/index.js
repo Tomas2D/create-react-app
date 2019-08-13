0e26fb4e81463fce4b0d2f8a52ca384ccd1c623e
Jiří Čermák <jiri.cermak@ackee.cz>
2019-08-13 14:36:14 +0200

    @ackee/react-scripts@1.0.0-alpha.7

--- packages/react-scripts/custom/scripts/publish/index.js
+++ packages/react-scripts/custom/scripts/publish/index.js
@@ -4,7 +4,7 @@ const fs = require('fs-extra');
 const { execSync } = require('child_process');
 const paths = require('../../config/paths');
 
-const { version } = fs.readSync(paths.packageJson);
+const { version } = fs.readJSONSync(paths.packageJson);
 const [tag] = version.match(/beta|alpha/) || ['latest'];
 
 execSync(`npm publish ${paths.temp.root} --tag ${tag} --dry-run`);
