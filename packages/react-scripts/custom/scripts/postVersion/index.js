fa2fc7b9e27ec0469f1b33dfcfb876139201fde6
Jiří Čermák <jiri.cermak@ackee.cz>
2019-08-13 13:04:50 +0200

    Fix postVersion script

--- packages/react-scripts/custom/scripts/postVersion/index.js
+++ packages/react-scripts/custom/scripts/postVersion/index.js
@@ -9,17 +9,13 @@ const ignore = {
   stdio: 'ignore',
 };
 
-try {
-  const { name, version } = fs.readJSONSync(paths.appPackageJson);
-  const tag = `${name}@${version}`;
+const { name, version } = fs.readJSONSync(paths.appPackageJson);
+const tag = `${name}@${version}`;
 
-  execSync('git add package.json', ignore);
-  execSync(`git commit -m "${tag}"`, ignore);
+execSync('git add package.json', ignore);
+execSync(`git commit -m "${tag}"`, ignore);
 
-  const lastCommitBuffer = execSync('git rev-parse HEAD');
-  const lastCommitHash = lastCommitBuffer.toString().replace('\n', '');
+const lastCommitBuffer = execSync('git rev-parse HEAD');
+const lastCommitHash = lastCommitBuffer.toString().replace('\n', '');
 
-  execSync(`git tag -a ${tag} ${lastCommitHash} -m "${tag}"`, ignore);
-} catch (e) {
-  console.error(e);
-}
+execSync(`git tag -a ${tag} ${lastCommitHash} -m "${tag}"`);
