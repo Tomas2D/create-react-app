cd3c683784341bb610e268cdf4c03eed895a78f0
Jiří Čermák <jiri.cermak@ackee.cz>
2019-08-29 00:47:04 +0200

    Update transformPackageJson

--- packages/react-scripts/custom/scripts/prepublish/transformPackageJson.js
+++ packages/react-scripts/custom/scripts/prepublish/transformPackageJson.js
@@ -4,6 +4,14 @@ const fs = require('fs-extra');
 const paths = require('../../config/paths');
 
 module.exports = async function transformPackageJson() {
+  if (!fs.existsSync(paths.packageJson)) {
+    throw new Error(`${paths.packageJson} file not found`);
+  }
+
+  if (!fs.existsSync(paths.customPackgeJson)) {
+    throw new Error(`${paths.customPackgeJson} file not found`);
+  }
+
   // transform react-scripts/package.json
   const [packageJson, customPackgeJson] = await Promise.all([
     fs.readJSON(paths.packageJson),
