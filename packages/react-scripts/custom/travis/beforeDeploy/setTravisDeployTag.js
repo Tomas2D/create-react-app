748dc7015a605e195ac35c3f9fc3947db89f876e
Jiří Čermák <jiri.cermak@ackee.cz>
2019-08-29 11:39:13 +0200

    Update travis before_deploy

--- packages/react-scripts/custom/travis/beforeDeploy/setTravisDeployTag.js
+++ packages/react-scripts/custom/travis/beforeDeploy/setTravisDeployTag.js
@@ -1,15 +1,9 @@
 'use strict';
 
 const fs = require('fs-extra');
-const { execSync } = require('child_process');
 const paths = require('../../config/paths');
 
 const { version } = fs.readJSONSync(paths.packageJson);
 const [tag] = version.match(/beta|alpha/) || ['latest'];
 
-try {
-  console.log(`[prepublish] Setting DEPLOY_TAG env. var. to ${tag}...`);
-  execSync(`export DEPLOY_TAG="${tag}"`);
-} catch (e) {
-  console.error(e);
-}
+console.log(tag);
