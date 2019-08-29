1f6d44be0983d62b995754ff159ee6891a8a28c1
Jiří Čermák <jiri.cermak@ackee.cz>
2019-08-29 10:33:18 +0200

    travis: set deploy.tag from env. variable

--- packages/react-scripts/custom/scripts/prepublish/index.js
+++ packages/react-scripts/custom/scripts/prepublish/index.js
@@ -1,16 +1,8 @@
 'use strict';
 
 require('./installDependencies');
+require('./setTravisDeployTag');
 
 const transformPackageJson = require('./transformPackageJson');
 
-(async function() {
-  try {
-    console.log(
-      '[prepublish] injecting custom scripts, dependencies to package.json...'
-    );
-    await transformPackageJson();
-  } catch (e) {
-    console.error(e);
-  }
-})();
+transformPackageJson().catch(console.error);
