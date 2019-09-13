bf4db849e75de792e181b7e249fa895b4b7e3855
Jiří Čermák <jiri.cermak@ackee.cz>
2019-09-13 15:58:29 +0200

    [test]: disable custom/scripts/init

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -23,12 +23,11 @@ const { defaultBrowsers } = require('react-dev-utils/browsersHelper');
 const os = require('os');
 const verifyTypeScriptSetup = require('./utils/verifyTypeScriptSetup');
 // @ackee/react-scripts - beginning
-const customPaths = require('../custom/config/paths');
-const {
-  gitCommitAmend,
-  modifyTemplatePackageJson,
-  installDependencies,
-} = require(path.resolve(customPaths.custom, 'scripts/init'));
+// const {
+//   gitCommitAmend,
+//   modifyTemplatePackageJson,
+//   installDependencies,
+// } = require('../custom/scripts/init');
 // @ackee/react-scripts - end
 
 function isInGitRepository() {
@@ -118,7 +117,7 @@ module.exports = async function(
   appPackage.browserslist = defaultBrowsers;
 
   // @ackee/react-scripts - beginning
-  await modifyTemplatePackageJson(ownPath, appPackage);
+  // await modifyTemplatePackageJson(ownPath, appPackage);
   // @ackee/react-scripts - end
 
   fs.writeFileSync(
@@ -236,14 +235,14 @@ module.exports = async function(
     console.log('Initialized a git repository.');
   }
 
-  await installDependencies(appPackage, {
-    useYarn,
-    verbose,
-  });
+  // await installDependencies(appPackage, {
+  //   useYarn,
+  //   verbose,
+  // });
 
   if (didGitInit) {
     // append changes to the last commit (the init commit) caused by installing postponed devDependencies
-    gitCommitAmend();
+    // gitCommitAmend();
   }
   // @ackee/react-scripts - end
 
