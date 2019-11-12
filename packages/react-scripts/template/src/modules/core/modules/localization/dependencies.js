714d1249d965dc2f3c05fa26723ce37cf5204c62
Jiří Čermák <jiri.cermak@ackee.cz>
2019-11-12 16:36:57 +0100

    Upgrade @ackee/jerome, @ackee/chris

--- packages/react-scripts/template/src/modules/core/modules/localization/dependencies.js
+++ packages/react-scripts/template/src/modules/core/modules/localization/dependencies.js
@@ -2,6 +2,6 @@ import * as translations from 'translations';
 
 export { translations };
 
-export { translatableWithAntdFactory, reducer as localizationReducerFactory } from '@ackee/jerome';
+export { translatableWithAntdFactory, reducerFactory } from '@ackee/jerome';
 export { default as csAntd } from 'antd/es/locale-provider/cs_CZ';
 export { strictObjectAccess } from '@ackee/redux-utils';
