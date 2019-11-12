ff56825f63a86134a30e4b10f81cbf443d6dea57
Jiří Čermák <jiri.cermak@ackee.cz>
2019-11-12 16:36:57 +0100

    Upgrade @ackee/jerome, @ackee/chris

--- packages/react-scripts/template/src/modules/core/modules/localization/services/reducer.js
+++ packages/react-scripts/template/src/modules/core/modules/localization/services/reducer.js
@@ -1,20 +1,4 @@
-import { localizationReducerFactory } from '../dependencies';
+import { reducerFactory } from '../dependencies';
 import { languages } from '../config';
 
-const getInitialLanguage = () => {
-    let language = languages[0];
-    const lang = window ? window.navigator.language : undefined;
-    const langSplitted = lang.split('-')[0];
-
-    if (languages.includes(lang)) {
-        language = lang;
-    } else if (languages.includes(langSplitted)) {
-        language = langSplitted;
-    }
-
-    return language;
-};
-
-const language = getInitialLanguage();
-
-export default localizationReducerFactory(language);
+export default reducerFactory(languages);
--- packages/react-scripts/template/src/modules/sharedDependencies.js
+++ packages/react-scripts/template/src/modules/sharedDependencies.js
@@ -6,10 +6,21 @@ import * as Consts from 'constants/index';
 export { PropTypes, sagaEffects, Log, Consts };
 
 export { default as React } from 'react';
-export { compose } from 'redux';
+export { compose, combineReducers } from 'redux';
 export { connect, useDispatch, useSelector } from 'react-redux';
-export { Route, Switch, Link, Redirect } from 'react-router-dom';
+export { Route, Switch, Link, Redirect, useLocation, useRouteMatch } from 'react-router-dom';
 export { FelaComponent, connect as connectFela } from 'react-fela';
 export { FormattedMessage, FormattedNumber, FromattedDate, useIntl } from 'react-intl';
 
+export { childrenPropType } from '@ackee/lucas';
+export {
+    strictObjectAccess,
+    createAsyncType,
+    containerReducer,
+    basicApiReducer,
+    apiSelector,
+} from '@ackee/redux-utils';
+export { takeLatestRequest, takeRequest } from '@ackee/antonio/es/saga-effects';
+
 export { default as Config } from 'config';
+export { authApi, api } from 'Config/antonio';
