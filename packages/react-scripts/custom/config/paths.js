7441594b2857601017a978b53d61e72e7c521b74
Jiří Čermák <jiri.cermak@ackee.cz>
2019-11-14 19:07:21 +0100

    Make eject work

--- packages/react-scripts/custom/config/paths.js
+++ packages/react-scripts/custom/config/paths.js
@@ -9,32 +9,45 @@ const resolveReactScriptsRoot = file => path.resolve(rootPath, file);
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveAppRoot = relativePath => path.resolve(appDirectory, relativePath);
 
-const paths = {
-  // @start: react-scripts context
-  customPackageJson: resolveReactScriptsRoot('custom/custom-package.json'),
+const paths = {};
+
+// Paths after eject
+Object.assign(paths, {
+  // start: react-scripts context
   customTransformImportPath: resolveReactScriptsRoot(
     'custom/config/plugins/transformImportPath.js'
   ),
+  webpackConfig: resolveAppRoot('config/webpack.config.js'),
+  // end: react-scripts context
 
-  prettierConfig: resolveReactScriptsRoot('template/prettier.config.js'),
-  templateSrc: resolveReactScriptsRoot('template/src'),
-
-  packageJson: resolveReactScriptsRoot('package.json'),
-
-  // TODO: won't work after eject, use resolveAppRoot then
-  webpackConfig: resolveReactScriptsRoot('config/webpack.config.js'),
-  // @end: react-scripts context
-
-  // @start: app context
+  // start: app context
   appWebpackConfig: resolveAppRoot('config/transformWebpackConfig.js'),
   appTransformImportPath: resolveAppRoot(
     'config/plugins/transformImportPath.js'
   ),
   appSrc: resolveAppRoot('src'),
+  // end: app context
+});
+
+// @remove-on-eject-beginning
+// Paths before eject
+Object.assign(paths, {
+  // start: react-scripts context
+  customPackageJson: resolveReactScriptsRoot('custom/custom-package.json'),
+  prettierConfig: resolveReactScriptsRoot('template/prettier.config.js'),
+  templateSrc: resolveReactScriptsRoot('template/src'),
+  packageJson: resolveReactScriptsRoot('package.json'),
+
+  webpackConfig: resolveReactScriptsRoot('config/webpack.config.js'),
+  // end: react-scripts context
+
+  // start: app context
+  appConfig: resolveAppRoot('config'),
   appPackageJson: resolveAppRoot('package.json'),
   appDisabledEslintrc: resolveAppRoot('eslintrc'),
   appEnabledEslintrc: resolveAppRoot('.eslintrc'),
-  // @end: app context
-};
+  // end: app context
+});
+// @remove-on-eject-end
 
 module.exports = paths;
